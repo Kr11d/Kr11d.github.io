5cc4a580e1e875436425f7de91e6351f894d8bdf
Kr11d <166440950+Kr11d@users.noreply.github.com>
2024-04-22 20:19:08 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -2,11 +2,11 @@ var app = new Vue({
     el: "#app",
     data: {
         products: [
-            { id: 1, title: "Prod 1", short_text: 'short_text', image: 'images/orange1.png', desc: "Full desc" },
-            { id: 2, title: "Prod 2", short_text: 'short_text', image: 'images/orange2.png', desc: "Full desc" },
-            { id: 3, title: "Prod 3", short_text: 'short_text', image: 'images/orange3.png', desc: "Full desc" },
-            { id: 4, title: "Prod 4", short_text: 'short_text', image: 'images/orange4.png', desc: "Full desc" },
-            { id: 5, title: "Prod 5", short_text: 'short_text', image: 'images/orange5.png', desc: "Full desc" }
+            { id: 1, title: "Navel Oranges", short_text: 'The navel orange is a sweet type of orange', image: 'images/orange1.png', desc: "Full desc" },
+            { id: 2, title: "Cara Cara Oranges", short_text: 'A type of navel orange and also seedless and sweet', image: 'images/orange2.png', desc: "Full desc" },
+            { id: 3, title: "Valencia Oranges", short_text: 'Valencia oranges are sweet and extremely juicy', image: 'images/orange3.png', desc: "Full desc" },
+            { id: 4, title: "Mandarin Orange", short_text: 'They are small, have a medium to thick loosely adherent rind', image: 'images/orange4.png', desc: "Full desc" },
+            { id: 5, title: "Clementines", short_text: 'Known for their small size and easy-to-peel bright orange skin', image: 'images/orange5.png', desc: "Full desc" }
         ]
     },
     computed: {
